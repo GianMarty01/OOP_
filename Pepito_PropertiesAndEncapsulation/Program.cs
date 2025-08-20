9e67ec4e18807bf76f687970f1c1ef8f2a9039f7
ACT-STUDENT <ACT-STUDENT@LAB3-PC40>
2025-08-20 10:29:32 +0800

    exercise

---
+++ Pepito_PropertiesAndEncapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pepito_PropertiesAndEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intantiate A shoe object
+            Shoe shoe1 = new Shoe();
+
+            //Setting Values of the properties
+            shoe1.Brand = "Adidas";
+            shoe1.Size = 9;
+
+            //Display shoe details
+            shoe1.DisplayShoeInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
