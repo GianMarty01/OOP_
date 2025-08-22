33dc0baa3a0f658d0ed420dd9200fff69c6d6645
ACT-STUDENT <ACT-STUDENT@LAB3-PC40>
2025-08-22 10:48:55 +0800

    asdasddas

---
+++ Pepito_EncapsulationUserInput/Pepito_EncapsulationUserInput/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pepito_EncapsulationUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //User Input Brand name of a shoe
+            Console.Write("Enter Brand Name: ");
+            string Brand = Console.ReadLine();
+            //User input of the shoe size
+            Console.Write("Enter Your Size: ");
+            int Size = Convert.ToInt32(Console.ReadLine()); ;
+            Console.WriteLine("");
+
+            //Intantiate A shoe object
+            Shoe shoe1 = new Shoe();
+
+            //Title
+            Console.WriteLine("------Shoe Display------");
+            shoe1.Brand = Brand;
+            shoe1.Size = Size;
+
+            //Display shoe details
+            shoe1.DisplayShoeInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pepito_EncapsulationUserInput/Pepito_EncapsulationUserInput/Shoe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pepito_EncapsulationUserInput
+{
+    class Shoe
+    {
+        //Gian Marty Pepito
+        //IT301
+        //Basics on Encapsulation
+
+        //Variables and fields
+        private string brand;
+        private int size;
+
+        //Making the public properties for the fields
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+
+        }
+
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+
+        }
+
+        //Method to display the shoe
+        public void DisplayShoeInfo()
+        {
+            Console.WriteLine($"Brand: {Brand} \nSize: {Size}");
+        }
+    }
+}
