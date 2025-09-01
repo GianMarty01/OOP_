f7b40c0cf9a4aa5d78c72eb5b8727d5f25c0c6a8
ACT-STUDENT <ACT-STUDENT@LAB3-PC40>
2025-09-01 10:55:13 +0800

    asd

---
+++ Pepito_SimpleInheritance/Pepito_SimpleInheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pepito_SimpleInheritance
+{
+    //Gian Marty Pepito
+    //IT301
+    //EXER_06: Simple Inheritance
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate an object
+            Student student = new Student();
+            student.Name = "Gian Marty Pepito";
+            student.Program = "BSIT";
+
+            //Instantiate an object of regular student
+            RegularStudent regularstudent = new RegularStudent();
+            regularstudent.Section = "IT301";
+            student.BasicINfo();                //Inherited Method
+            regularstudent.SectionEnrolled();         //Method for Regular student class
+
+            Console.WriteLine("");
+
+            //Instantiate an object of irregular student
+            IrregularStudent irregularstudent2 = new IrregularStudent();
+            irregularstudent2.Name = "Aaron Ordeniza";
+            irregularstudent2.Program = "BSIT";
+            irregularstudent2.UnitsEnrolled = 20;
+            irregularstudent2.BasicINfo();                //Inherited Method
+            irregularstudent2.EnrolledSemUnits();        //Method for IrregularStudent class
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pepito_SimpleInheritance/Pepito_SimpleInheritance/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pepito_SimpleInheritance
+{
+    class Student      //Base Class
+    {
+        public string Name { get; set; }        //Properties
+        public string Program { get; set; }
+
+        public void BasicINfo()         //Method for Student class
+        {
+            Console.WriteLine("Name: " + Name  );
+            Console.WriteLine("Program: " + Program);
+        }
+    }
+
+    class RegularStudent : Student      //Derived Class
+    {
+        public string Section { get; set; }        //Properties
+
+        public void SectionEnrolled()         //Method for Regular student class
+        {
+            Console.WriteLine("SectionEnrolled: " + Section);
+        }
+    }
+
+    class IrregularStudent : Student        
+    {
+        public int UnitsEnrolled { get; set; }        //Properties
+
+        public void EnrolledSemUnits()         //Method for IrregularStudent class
+        {
+            Console.WriteLine("EnrolledSemUnits: " + UnitsEnrolled);
+        }
+    }
+}
