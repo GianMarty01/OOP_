3359183fb1127d8671bc4000479c191968898039
ACT-STUDENT <ACT-STUDENT@LAB3-PC40>
2025-09-03 10:50:26 +0800

    asdasd

---
+++ CONSTRUCTOR/CONSTRUCTOR/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSTRUCTOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student student1 = new Student("Gian", "BSIT");
+            //student1.DisplayStudentBasicinfo();
+            Console.WriteLine("----Regular Student----");
+            RegularStudent student1 = new RegularStudent("IT301", "Gian Dumdum", "BSIT");
+            student1.RegularStudentSection();
+
+            Console.WriteLine();
+
+            Console.WriteLine("----Irregular Student----");
+            IrregularStudent Student2 = new IrregularStudent(20, "Aaron Ordeniza", "BSIT", "IT301");
+            Student2.IrregularStudentUnits();
+
+
+        }
+    }
+}
---
+++ CONSTRUCTOR/CONSTRUCTOR/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CONSTRUCTOR
+{
+    class Student
+    {
+        private string name;
+        private string program;
+
+        public Student(string aName, string aProgram)
+        {
+            name = aName;
+            program = aProgram;
+        }
+
+        public void DisplayStudentBasicinfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Program: " + program);
+        }
+
+    }
+    class RegularStudent : Student
+    {
+        public string Section;
+        public RegularStudent(string aSection, string aName, string aProgram) : base(aName, aProgram)
+        {
+            Section = aSection;
+        }
+        public void RegularStudentSection()
+        {
+            DisplayStudentBasicinfo();
+            Console.WriteLine("Section: " + Section);
+        }
+    }
+    class IrregularStudent : Student
+    {
+        public int UnitsEnrolled;
+        public IrregularStudent(int aUnitsEnrolled, string aName, string aProgram, string aSection) : base(aName, aProgram)
+        {
+            UnitsEnrolled = aUnitsEnrolled;
+        }
+        public void IrregularStudentUnits()
+        {
+            DisplayStudentBasicinfo();
+            Console.WriteLine("Units Enrolled: " + UnitsEnrolled);
+        }
+
+    }
+}
