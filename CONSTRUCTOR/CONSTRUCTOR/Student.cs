8dd107c0dcb7bc7c733a83d0a549f30c1b73f351
GianMarty01 <allenmercado2005@gmail.com>
2025-09-05 10:49:51 +0800

    Update Student.cs

--- CONSTRUCTOR/CONSTRUCTOR/Student.cs
+++ CONSTRUCTOR/CONSTRUCTOR/Student.cs
@@ -8,49 +8,53 @@ using System.Xml.Linq;
 
 namespace CONSTRUCTOR
 {
-    class Student
-    {
-        private string name;
-        private string program;
+     class Student    // Base class
+ {
+     // Fields
+     private string name;
+     private string program;
+     //Parametarized Constructor 
+     public Student(string aName, string aProgram)      
+     {
+         name = aName;       // Set Name
+         program = aProgram;     //Set Program
+     }
 
-        public Student(string aName, string aProgram)
-        {
-            name = aName;
-            program = aProgram;
-        }
+     public void Basicinfo()     // Method to display student basic info
+     {
+         Console.WriteLine("Name: " + name);     // Show Name
+         Console.WriteLine("Program: " + program);       // Show Program
+     }
+ }
+ class RegularStudent : Student     // Derived class
+ {
+     public string section;    // Field
 
-        public void DisplayStudentBasicinfo()
-        {
-            Console.WriteLine("Name: " + name);
-            Console.WriteLine("Program: " + program);
-        }
+     // Parametarized Constructor
+     public RegularStudent(string aSection, string aName, string aProgram) : base(aName, aProgram)     
+     {
+         section = aSection;      // Set section
+     }
+     public void SectionEnrolled()       // Method to diplay Regular student units
+     {  
+         Basicinfo();    // Inheriting the student basic info || getting the name and program
+         Console.WriteLine("Section: " + section);       //Show Section
+     }
+ }
+ class IrregularStudent : Student      // Derived class
+ {
+     public int unitsEnrolled;       // Field
 
-    }
-    class RegularStudent : Student
-    {
-        public string Section;
-        public RegularStudent(string aSection, string aName, string aProgram) : base(aName, aProgram)
-        {
-            Section = aSection;
-        }
-        public void RegularStudentSection()
-        {
-            DisplayStudentBasicinfo();
-            Console.WriteLine("Section: " + Section);
-        }
-    }
-    class IrregularStudent : Student
-    {
-        public int UnitsEnrolled;
-        public IrregularStudent(int aUnitsEnrolled, string aName, string aProgram, string aSection) : base(aName, aProgram)
-        {
-            UnitsEnrolled = aUnitsEnrolled;
-        }
-        public void IrregularStudentUnits()
-        {
-            DisplayStudentBasicinfo();
-            Console.WriteLine("Units Enrolled: " + UnitsEnrolled);
-        }
+     //Parametarized Constructor
+     public IrregularStudent(int aUnitsEnrolled, string aName, string aProgram, string aSection) : base(aName, aProgram) 
+     {
+         unitsEnrolled = aUnitsEnrolled;     // Set enrolled units
+     }
+     public void EnrolledSemUnits()      // Method to diplay Irregular student units
+     {
+         Basicinfo();    // Inheriting the student basic info || getting the name and program
+         Console.WriteLine("Units Enrolled: " + unitsEnrolled);      // Show Units enrolled
+     }
 
-    }
+ }
 }
