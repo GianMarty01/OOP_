d21f3a69e4ed16189ddae5e621c1b9ade72ba607
GianMarty01 <allenmercado2005@gmail.com>
2025-09-05 10:50:15 +0800

    Update Program.cs

--- CONSTRUCTOR/CONSTRUCTOR/Program.cs
+++ CONSTRUCTOR/CONSTRUCTOR/Program.cs
@@ -7,22 +7,34 @@ using System.Threading.Tasks;
 namespace CONSTRUCTOR
 {
     class Program
+{
+    //Gian MArty Pepito
+    //IT301
+    //EXER_07: Simple Inheritance with Constructor
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            //Student student1 = new Student("Gian", "BSIT");
-            //student1.DisplayStudentBasicinfo();
-            Console.WriteLine("----Regular Student----");
-            RegularStudent student1 = new RegularStudent("IT301", "Gian Dumdum", "BSIT");
-            student1.RegularStudentSection();
+        // Print regular student above student info
+        Console.WriteLine("----Regular Student----");
 
-            Console.WriteLine();
+        // Instantiate  an object to show the parametarized constructor
+        RegularStudent student1 = new RegularStudent("IT301", "Gian Dumdum", "BSIT");
 
-            Console.WriteLine("----Irregular Student----");
-            IrregularStudent Student2 = new IrregularStudent(20, "Aaron Ordeniza", "BSIT", "IT301");
-            Student2.IrregularStudentUnits();
+        // Display regular student info with inherited student student info
+        student1.SectionEnrolled(); 
 
+        Console.WriteLine();    // Spacing
 
-        }
+        // Print irregular student above student info
+        Console.WriteLine("----Irregular Student----");
+
+        // Instantiate an object to show the parametarized constructor
+        IrregularStudent Student2 = new IrregularStudent(20, "Aaron Ordeniza", "BSIT", "IT301");
+
+        // Display irregular student info with inherited student info
+        Student2.EnrolledSemUnits();
+
+        Console.ReadKey();
     }
+}
 }
