b6dce107d15cce41ee89401476f3d455db872bd5
ACT-STUDENT <ACT-STUDENT@LAB3-PC40>
2025-09-08 10:36:32 +0800

    asd

---
+++ Pepito_UserInputInheritance/Pepito_UserInputInheritance/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pepito_UserInputInheritance
+{
+    class Program
+    {
+        //Gian Marty Pepito
+        //IT301
+        //Exer 8: 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Enter Shoe info to display-----");
+            Console.Write("Enter Brand Name: ");
+            string Brand = Console.ReadLine();
+
+            Console.Write("Enter Size: ");
+            int Size = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Color: ");
+            string Color = Console.ReadLine();
+
+
+            //Intantiate A shoe object
+            Shoe shoe1 = new Shoe();
+
+            //Setting Values of the properties
+            shoe1.Brand = Brand;
+            shoe1.Size = Size;
+
+            //Calling the method to Display shoe details
+            shoe1.DisplayShoeInfo();
+
+            Adidas style = new Adidas();
+
+            style.Color = Color;
+
+            style.DisplayShoeInfo();
+            style.DisplayColor();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pepito_UserInputInheritance/Pepito_UserInputInheritance/Shoe.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pepito_UserInputInheritance
+{
+    class Shoe      // Base class
+    {
+        //private Variables and fields
+        private string brand;
+        private int size;
+
+        //Making the public properties for the fields
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+
+        }
+
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+
+        }
+
+        //Method to display the shoe
+        public void DisplayShoeInfo()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("------Shoe Display------");
+            Console.WriteLine($"Brand: {Brand} \nSize: {Size}");
+        }
+    }
+    class Adidas : Shoe
+    {
+        private string color;
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public void DisplayColor()
+        {
+            DisplayShoeInfo();
+            Console.WriteLine("Color: " + Color);
+        }
+    }
+}
