437c0799ae29a7c454c2ca09bde4804d497243e6
ACT-STUDENT <ACT-STUDENT@LAB3-PC40>
2026-02-04 08:33:37 +0800

    POLYMORPH ONLY

---
+++ Polymorph_with_Userinput/Polymorph_with_Userinput/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorph_with_Userinput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create objects
+            Sport sport = new Sport("Adidas", 9, "Adizero Boston");
+            Formal formal = new Formal("Louis Philippe", 10, "Leather");
+
+            // Display Information
+            sport.DisplayInfo();
+            formal.DisplayInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Polymorph_with_Userinput/Polymorph_with_Userinput/Shoe.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorph_with_Userinput
+{
+    class Shoe
+    {
+        public string Brand { get; set; } // public properties
+        public double Size { get; set; }
+
+        public Shoe(string brand, double size) // Parameterized constructor
+        {
+            Brand = brand;
+            Size = size;
+        }
+
+        public virtual void DisplayInfo() // Virtual Method
+        {
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Size: {Size}");
+        }
+    }
+    class Sport : Shoe // Derived class
+    {
+        public string Model { get; set; } // Properties
+                                          // Parameterized constructor
+        public Sport(string brand, double size, string model) : base(brand, size)
+        {
+            Model = model;
+        }
+
+        public override void DisplayInfo() // Override Method
+        {
+            Console.WriteLine("--- Sport Details ---");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Size: {Size}");
+        }
+    }
+    class Formal : Shoe // Derived class
+    {
+        public string Type { get; set; } // Properties
+                                         // Parameterized constructor
+        public Formal(string brand, double size, string type) : base(brand, size)
+        {
+            Type = type;
+        }
+
+        public override void DisplayInfo() // Override Method
+        {
+            Console.WriteLine("--- Formal Shoe details ---");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Type: {Type}");
+            Console.WriteLine($"Size: {Size}");
+        }
+    }
+}
